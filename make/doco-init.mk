4b951774a14e0c83cc4a4a43d9ccaf5636991d73
Yuriy Olkhovyy <y.olkhovyy@gmail.com>
2025-03-27 17:47:38 +0100

    Rename otel collector configs

--- make/doco-init.mk
+++ make/doco-init.mk
@@ -1,36 +1,36 @@
-export COMPOSE_DOCKER_CLI_BUILD = 1
 export DOCKER_BUILDKIT = 1
+export COMPOSE_DOCKER_CLI_BUILD = 1
 export COMPOSE_BAKE = true
 
 DOCO = docker compose -f docker-compose.yml
 export DEPENDENCIES = otel-collector
 
 ifdef NR		#----- newrelic
-	export OTEL_COLLECTOR_CONFIG = config-newrelic.yml
+	export OTEL_COLLECTOR_CONFIG = newrelic.yml
 else ifdef DD	#----- datadog
-	export OTEL_COLLECTOR_CONFIG = config-datadog.yml
+	export OTEL_COLLECTOR_CONFIG = datadog.yml
 else ifdef UPT	#----- uptrace
 	DOCO := $(DOCO) -f docker-compose.uptrace.yml
 	DEPENDENCIES := $(DEPENDENCIES) postgres clickhouse uptrace
-	export OTEL_COLLECTOR_CONFIG = config-uptrace.yml
+	export OTEL_COLLECTOR_CONFIG = uptrace.yml
 else ifdef EK	#----- elastic, kibana
 	DOCO := $(DOCO) -f docker-compose.elastic-kibana.yml
 	DEPENDENCIES := $(DEPENDENCIES) elasticsearch kibana
-	export OTEL_COLLECTOR_CONFIG = config-elastic-kibana.yml
+	export OTEL_COLLECTOR_CONFIG = elastic-kibana.yml
 else ifdef GCL	#----- grafana cloud
-	export OTEL_COLLECTOR_CONFIG = config-grafana-cloud.yml
+	export OTEL_COLLECTOR_CONFIG = grafana-cloud.yml
 else ifdef ALY	#----- grafana cloud alloy
 	DOCO := $(DOCO) -f docker-compose.grafana-cloud-alloy.yml
 	DEPENDENCIES := $(DEPENDENCIES) alloy
-	export OTEL_COLLECTOR_CONFIG = config-grafana-cloud-alloy.yml
+	export OTEL_COLLECTOR_CONFIG = grafana-cloud-alloy.yml
 else ifdef GLT	#----- grafana loki, tempo, prometheus
 	DOCO := $(DOCO) -f docker-compose.grafana-loki-tempo.yml
 	DEPENDENCIES := $(DEPENDENCIES) grafana loki tempo prometheus promtail
-	export OTEL_COLLECTOR_CONFIG = config-grafana-loki-tempo.yml
+	export OTEL_COLLECTOR_CONFIG = grafana-loki-tempo.yml
 else			#----- grafana loki, jaeger, prometheus
 	DOCO := $(DOCO) -f docker-compose.grafana-loki-jaeger.yml
 	DEPENDENCIES := $(DEPENDENCIES) grafana loki jaeger prometheus promtail
-	export OTEL_COLLECTOR_CONFIG = config-grafana-loki-jaeger.yml
+	export OTEL_COLLECTOR_CONFIG = grafana-loki-jaeger.yml
 endif
 
 export SERVICES = $(filter-out ${DEPENDENCIES}, $(shell ${DOCO} config --services))
